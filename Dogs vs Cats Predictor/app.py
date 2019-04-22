06c115907f3fb1a39da6d33e15778dc87516d44e
aditya-67 <adityagannavarapu.67@gmail.com>
2019-04-22 10:30:07 +0530

    Music Generation

--- Dogs vs Cats Predictor/app.py
+++ Dogs vs Cats Predictor/app.py
@@ -52,7 +52,7 @@ def predict():
 
 if __name__ == "__main__":
 	#decide what port to run the app in
-	port = int(os.environ.get('PORT', 5000))
+	port = int(os.environ.get('PORT', 8080))
 	#run the app locally on the givn port
 	app.run(host='0.0.0.0', port=port)
 	#optional if we want to run in debugging mode
